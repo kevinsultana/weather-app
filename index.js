2cb1179e37a92d56881531011d1670f95dae5992
kevinsultana <kevinpdg.ks@gmail.com>
2025-06-05 21:33:21 +0700

    feat: added static fetch weather data

--- index.js
+++ index.js
@@ -18,3 +18,61 @@ const toggleDark = document.getElementById("toggle-dark");
 toggleDark.addEventListener("change", function () {
   document.documentElement.classList.toggle("dark");
 });
+
+const urlCurrent =
+  "https://api.open-meteo.com/v1/forecast?latitude=-6.18&longitude=106.8223&current=temperature_2m,is_day,weather_code&timezone=auto";
+
+const urlDaily =
+  "https://api.open-meteo.com/v1/forecast?latitude=-6.18&longitude=106.8223&daily=weather_code,temperature_2m_max,temperature_2m_min,uv_index_max,uv_index_clear_sky_max,wind_direction_10m_dominant,wind_speed_10m_max,sunrise,sunset&timezone=auto&forecast_days=1";
+
+const formatter = new Intl.DateTimeFormat("id-ID", {
+  day: "numeric",
+  month: "long",
+  year: "numeric",
+});
+
+const formatDate = (date) => {
+  return formatter.format(date);
+};
+
+const formatDay = (date) => {
+  return date.toLocaleDateString("id-ID", { weekday: "long" });
+};
+
+const getDataCurrent = async () => {
+  try {
+    const response = await fetch(urlCurrent);
+    const dataCurrent = await response.json();
+    // console.log(dataCurrent);
+    const currentLocation = document.getElementById("current-location");
+    currentLocation.textContent = "Jakarta";
+
+    const currentDate = document.getElementById("current-date");
+    currentDate.textContent = formatDate(new Date(dataCurrent.current.time));
+
+    const currentDay = document.getElementById("current-day");
+    currentDay.textContent = formatDay(new Date(dataCurrent.current.time));
+
+    const currentTemp = document.getElementById("current-temp");
+    currentTemp.textContent =
+      dataCurrent.current.temperature_2m +
+      dataCurrent.current_units.temperature_2m;
+
+    const responseDaily = await fetch(urlDaily);
+    const dataDaily = await responseDaily.json();
+    console.log(dataDaily);
+
+    const currentTempRange = document.getElementById("current-temp-range");
+    currentTempRange.textContent =
+      "High: " +
+      dataDaily.daily.temperature_2m_max[0] +
+      dataDaily.daily_units.temperature_2m_max +
+      " Low: " +
+      dataDaily.daily.temperature_2m_min[0] +
+      dataDaily.daily_units.temperature_2m_min;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getDataCurrent();
