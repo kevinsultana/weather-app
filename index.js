1832efcae7cc3cb5a27c3c8b5eabe0c7047f6a6e
kevinsultana <kevinpdg.ks@gmail.com>
2025-06-10 21:47:42 +0700

    fix: url forecast

--- index.js
+++ index.js
@@ -324,7 +324,7 @@ const closeModalAllCity = () => {
 };
 
 const urlDataOtherCities = (countryId) => {
-  return `http://api.geonames.org/searchJSON?country=${countryId}&featureClass=P&maxRows=12&orderby=population&username=kevinsul22`;
+  return `https://api.geonames.org/searchJSON?country=${countryId}&featureClass=P&maxRows=12&orderby=population&username=kevinsul22`;
 };
 
 let dataOtherCities = [];
